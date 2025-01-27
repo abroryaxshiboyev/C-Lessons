fd584af043f3bc03c514728b293b9a567f7482f1
Abror <abroryaxshiboyev2002@gmail.com>
2025-01-27 14:47:35 +0500

    Methods

---
+++ LessonMethods/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using LessonMethods;
+
+RealEstate elmStreet=new()
+{
+    Address="Elm Street",
+    SquareFootage=1300,
+    Price=300000,
+};
+Console.WriteLine(elmStreet.CalculatePricePerSquareFoot());
