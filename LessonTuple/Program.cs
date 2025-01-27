8895de2c62196f2cbc4938da9944e6e2f383fed1
Abror <abroryaxshiboyev2002@gmail.com>
2025-01-27 16:56:37 +0500

    Tuple

---
+++ LessonTuple/Program.cs
@@ -0,0 +1,21 @@
+﻿var values=("t",2,"p");
+System.Console.WriteLine(values.Item1);
+System.Console.WriteLine(values.Item2);
+System.Console.WriteLine(values.Item3);
+
+var valuesWithName=(First:"t",Second:2,Third:"p");
+
+System.Console.WriteLine(valuesWithName.First);
+System.Console.WriteLine(valuesWithName.Second);
+System.Console.WriteLine(valuesWithName.Third);
+
+(int a,string b,bool c) ReturnTheseValues()
+{
+    return (10,"Hello",true);
+}
+
+(int a,string b,bool c)=ReturnTheseValues();
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
