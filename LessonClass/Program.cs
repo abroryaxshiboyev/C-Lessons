3f8f4d2b63b4a21f622b8bf75bf4a1b7cfc8b529
Abror <abroryaxshiboyev2002@gmail.com>
2025-01-27 11:52:42 +0500

    Class

---
+++ LessonClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using LessonClass;
+
+//CRUD
+
+Rat wolfRat=new Rat();
+Rat fancyRat=new Rat();
+
+wolfRat.Name="Wolf Rat";
+wolfRat.Number = 1000;
+wolfRat.IsRadioactive=false;
+
+fancyRat.Name="Fancy Rat";
+fancyRat.Number = 500;
+fancyRat.IsRadioactive=false;
+
+//Read
+System.Console.WriteLine(wolfRat.Name);
+System.Console.WriteLine(fancyRat.Name);
+
+//Update
+wolfRat.Name="Updated Wolf Rat";
+fancyRat.Name="Updated Fancy Rat";
+
+Console.WriteLine(wolfRat.Name);
+Console.WriteLine(fancyRat.Name);
+
+//Delete
+wolfRat.Name="";
+fancyRat.Name="";
+
+Console.WriteLine(wolfRat.Name);
+Console.WriteLine(fancyRat.Name);
